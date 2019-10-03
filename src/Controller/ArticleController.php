ad63b0c647a00a79b59234e0c554ec9d3016c693
Mankyun Choi <ferozah83@gmail.com>
2019-10-03 17:05:17 +1300

    Symfony4 : Database & Doctrine ORM

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,20 +1,53 @@
 <?php
- namespace App\Controller;
 
- use Symfony\Component\HttpFoundation\Response;
- use Symfony\Component\Routing\Annotation\Route;
- use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
- use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
- class ArticleController extends Controller {
-     /**
-      * @Route("/")
-      * @Method({"GET"})
-      */
-     public function index(){
-        // return new Response('<html><body>Hello</body></html>');
-        $articles = ['Article 1', 'Article 2'];
-        return $this->render('articles/index.html.twig', array('articles'=>'$articles'));
-     }
- }
+class ArticleController extends Controller
+{
+    /**
+     * @Route("/", name="article_list")
+     * @Method({"GET"})
+     */
+    public function index()
+    {
+
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+        return $this->render('articles/index.html.twig', array('articles' => $articles));
+    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+
+    public function show($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', array('article'=> $article));
+    }
+
+    /**
+     * @Route("/article/save")
+     * 
+     */
+
+//    public function save()
+//    {
+//        $entityManager = $this->getDoctrine()->getManager();
+//        $article = new Article();
+//        $article->setTitle('Article One');
+//        $article->setBody('This is the body for article one');
+//
+//        $entityManager->persist($article);
+//
+//        $entityManager->flush();
+//
+//        return new Response('Saved an article with the id of ' . $article->getId());
+//    }
+}
